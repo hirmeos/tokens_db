d9d6b8b903f6a57d9173ded1879e799d412712a9
Javier Arias <javier@arias.re>
2018-08-29 15:05:18 +0100

    add authority

--- sql/10-schema.sql
+++ sql/10-schema.sql
@@ -1,9 +1,14 @@
+CREATE TABLE authority(
+  authority varchar(255) PRIMARY KEY NOT NULL
+);
+
 CREATE TABLE account(
   account_id varchar(255) PRIMARY KEY NOT NULL,
   name varchar(255) NOT NULL,
   surname varchar(255) NOT NULL,
   email varchar(255) UNIQUE NOT NULL,
-  password varchar(255) NOT NULL
+  password varchar(255) NOT NULL,
+  authority varchar(255) NOT NULL REFERENCES authority(authority) ON UPDATE CASCADE
 );
 
 CREATE TABLE token (
